6a30217636fc5bb106c1289cf52259e0bd3acf15
Zekum <129169517+Zekum@users.noreply.github.com>
2023-03-28 11:22:12 +0100

    Create DeadmansCave.cs

---
+++ DeadmansCave.cs
@@ -0,0 +1,83 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.World.Generation;
+
+namespace MyMod
+{
+    public class MyWorld : ModWorld
+    {
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            // Find the index of the Granite Cave pass
+            int graniteIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Granite Caves"));
+
+            // Insert a new pass after the Granite Cave pass
+            if (graniteIndex != -1)
+            {
+                tasks.Insert(graniteIndex + 1, new PassLegacy("Deadmans Cave", GenerateDeadmansCave));
+            }
+        }
+
+        private void GenerateDeadmansCave(GenerationProgress progress)
+        {
+            // Set the progress message
+            progress.Message = "Creating Deadmans Cave";
+
+            // Choose a random position in the underground layer
+            int x = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
+            int y = WorldGen.genRand.Next((int)WorldGen.rockLayer, Main.maxTilesY - 200);
+
+            // Dig a cave using a worm algorithm
+            WorldGen.TileRunner(x, y, WorldGen.genRand.Next(100, 200), WorldGen.genRand.Next(50, 100), TileID.Stone);
+
+            // Place a deadmans chest at the end of the cave
+            int chestX = x;
+            int chestY = y;
+
+            // Find the lowest solid tile in the cave
+            while (!WorldGen.SolidTile(chestX, chestY) && chestY < Main.maxTilesY - 50)
+            {
+                chestY++;
+            }
+
+            // Place the chest on top of the solid tile
+            WorldGen.PlaceTile(chestX, chestY - 1, TileID.Containers);
+            
+            // Get the chest index
+            int chestIndex = Chest.FindChest(chestX, chestY - 1);
+
+            // If the chest is valid, set its type to deadmans chest and add some loot
+            if (chestIndex >= 0)
+            {
+                Main.chest[chestIndex].chestStyle = ChestStyleID.DeadMansChest;
+
+                // Add some random loot to the chest
+                Item[] loot = new Item[]
+                {
+                    new Item { type = ItemID.GoldCoin, stack = WorldGen.genRand.Next(5, 11) },
+                    new Item { type = ItemID.Grenade, stack = WorldGen.genRand.Next(10, 21) },
+                    new Item { type = ItemID.Dynamite, stack = WorldGen.genRand.Next(3, 7) },
+                    new Item { type = ItemID.SilverBullet, stack = WorldGen.genRand.Next(50, 101) },
+                    new Item { type = ItemID.GoldenKey },
+                    new Item { type = ItemID.BoneWelder }
+                };
+
+                // Shuffle the loot array
+                for (int i = 0; i < loot.Length; i++)
+                {
+                    int j = WorldGen.genRand.Next(i, loot.Length);
+                    Item temp = loot[i];
+                    loot[i] = loot[j];
+                    loot[j] = temp;
+                }
+
+                // Fill the chest with the loot
+                for (int i = 0; i < loot.Length; i++)
+                {
+                    Main.chest[chestIndex].item[i] = loot[i];
+                }
+            }
+        }
+    }
+}
